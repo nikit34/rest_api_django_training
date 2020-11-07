f90197d72f661bd489baefd1609c40b3b4aa4925
Никита Пермяков <permikov134@mail.ru>
2020-11-08 00:46:18 +0300

    add login template

--- restlibrary/settings.py
+++ restlibrary/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
     'snippets',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -91,10 +92,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    )
 }
 
 # Internationalization
--- restlibrary/urls.py
+++ restlibrary/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.views import login
 from django.urls import path, include
+from rest_framework.authtoken import views
 from .router import router
 
 
@@ -7,4 +9,6 @@ urlpatterns = [
     path('snippets/', include('snippets.urls', namespace='snippets')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('login/', login, name='login'),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
 ]
