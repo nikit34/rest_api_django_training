b13aab2cf4cf0faaa94d3ac6eb7393a577f4c189
Никита Пермяков <permikov134@mail.ru>
2020-11-08 00:26:39 +0300

    refactoring view set

--- snippets/api/viewsets.py
+++ snippets/api/viewsets.py
@@ -1,7 +1,9 @@
 from snippets.models import Snippet
-from .serializers import SnippetSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
+
+from .serializers import SnippetSerializer
 
 
 # class SnippetViewSet(viewsets.ViewSet):
@@ -16,3 +18,9 @@ from rest_framework.response import Response
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    @action(methods=['get'], detail=False)
+    def newest(self, request):
+        newest = self.get_queryset().order_by('created').last()
+        serializer = self.get_serializer_class()(newest)
+        return Response(serializer.data)
